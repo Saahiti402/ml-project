b43402bb41530843c958c3fb5fc565dd28c9f203
Saahiti402 <kssaahitimbl@gmail.com>
2025-04-28 05:27:30 +0530

    current state - viz does not exit

--- src/ml_project/pipeline_registry.py
+++ src/ml_project/pipeline_registry.py
@@ -2,6 +2,7 @@ from kedro.pipeline import Pipeline
 from ml_project.pipelines.data_processing import pipeline as dp
 
 def register_pipelines() -> dict[str, Pipeline]:
+    """Register the project's pipelines."""
     return {
-        "__default__": dp.create_pipeline(),
+        "__default__": dp.create_pipeline(),  # Register the data_processing pipeline as the default pipeline
     }
